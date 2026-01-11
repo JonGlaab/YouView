8e0726b35ead99285fe0007866cf46f8bf223b55
ElizabethMokrusa <elimokrusa@msn.com>
2026-01-11 16:59:45 -0500

    Updated packages

--- YouView/Program.cs
+++ YouView/Program.cs
@@ -2,13 +2,21 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using YouView.Data;
 using YouView.Models;
+using Azure.Storage.Blobs;
 
 var builder = WebApplication.CreateBuilder(args); 
 
 var connectionString = builder.Configuration.GetConnectionString("YouViewDbConnection") 
                        ?? throw new InvalidOperationException("Connection string 'YouViewDbConnection' not found.");
+
+// Read the string from appsettings
+var blobConnectionString = builder.Configuration.GetSection("AzureStorage")["ConnectionString"];
+
 builder.Services.AddDbContext<YouViewDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// Register the BlobServiceClient so you can use it in your Upload page
+builder.Services.AddSingleton(x => new BlobServiceClient(blobConnectionString));
 
 builder.Services.AddDefaultIdentity<User>(options => {
         options.SignIn.RequireConfirmedAccount = false; // Todo: change to true after and remove this part
