04b1703722af3a0a2a7ac2129164932fe3379977
LouisV-MT <traceur.vu96@gmail.com>
2026-01-13 21:20:10 -0500

    Add Duration property to Video model and database
    Introduced a new 'Duration' column to the Videos table via EF Core migration and updated the Video model to include a required Duration property. This change allows storing and retrieving video duration information.

---
+++ YouView/Migrations/20260114002246_AddDurationColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YouView.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDurationColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Duration",
+                table: "Videos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Videos");
+        }
+    }
+}
